20a5b89e9885cad2155026aca6ed547e1bf55981
SANDIP KUMAR DUBEY <149190295+sandipkumardubey91@users.noreply.github.com>
2024-07-07 23:58:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPasswor
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCgXcZRCuc7mpbnWCvqnDqsdaABYj31Zi8",
-  authDomain: "quora-718e0.firebaseapp.com",
-  projectId: "quora-718e0",
-  storageBucket: "quora-718e0.appspot.com",
-  messagingSenderId: "918088771769",
-  appId: "1:918088771769:web:ae627076aaae101e869368"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
