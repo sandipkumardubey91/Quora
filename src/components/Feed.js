92a911ab9d8d85001c041df960cdc1c2c71658ea
sandipkumardubey91 <contactsandipkdubey1@gmail.com>
2024-07-10 22:30:29 +0530

    update

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,44 +1,4 @@
-// import React, { useEffect, useState } from 'react'
-// import './Feed.css'
-// import QuoraBox from './QuoraBox'
-// import Post from './Post'
 
-// const Feed = () => {
-//   const [posts, setPosts] = useState([]);
-//   useEffect(() => {
-//     db.collection("questions").orderBy("timestamp", "desc").onSnapshot(snapshot => setPosts(snapshot.docs.map((doc) => (({
-//       id : doc.id,
-//       question : doc.data()
-//     })))))
-//   },[])
-//   return (
-//     <div className='feed'>
-//         <QuoraBox/>
-//         {
-//           posts.map(({id, question}) =>{
-//             <Post 
-//               key = {id}
-//               id = {id}
-//               image = {question.imageUrl}
-//               question = {question.question}
-//               timestamp = {question.timestamp} 
-//               quoraUser = {question.user}
-//             />
-//           })
-//         }
-//         <Post/>
-//         <Post/>
-//         <Post/>
-//         <Post/>
-//         <Post/>
-//         <Post/>
-
-  
-//     </div>
-//   )
-// }
-
-// export default Feed
 
 import React, { useEffect, useState } from 'react';
 import './Feed.css';
